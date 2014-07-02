8ef94f244260ce0a9335d5aff9c305109cb83d59
hemanth <hemanth.hm@gmail.com>
2014-07-02 08:25:29 +0000

    Readme and others

---
+++ start.sh
@@ -0,0 +1,5 @@
+export HUBOT_IRC_SERVER=irc.freenode.net   
+export HUBOT_IRC_ROOMS="#perl-cats"   
+export HUBOT_IRC_NICK="huby"   
+export HUBOT_IRC_UNFLOOD="true"   
+./bin/hubot -a irc --name huby
